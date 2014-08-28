369b805c4a6df5a3b2786409477dc7bc97afd0a6
Fan Jianxiang <sevengram1991@gmail.com>
2014-08-28 19:55:50 +0800

    1. Add referer in headers 2. Change cookie rules

--- deepsky/util/wxclient.py
+++ deepsky/util/wxclient.py
@@ -2,33 +2,52 @@
 
 import urlparse
 import urllib
-import tornado.web
 import tornado.gen
 import tornado.httpclient
-import tornado.curl_httpclient
 import tornado.httputil
 import json
 import Cookie
 import time
 import sys
+import random
 
 from bs4 import BeautifulSoup
 
-login_url = 'https://mp.weixin.qq.com/cgi-bin/login'
-send_url = 'https://mp.weixin.qq.com/cgi-bin/singlesend'
-message_url = 'https://mp.weixin.qq.com/cgi-bin/message'
+base_url = 'https://mp.weixin.qq.com/'
+
+home_url = base_url + 'cgi-bin/home'
+
+login_url = base_url + 'cgi-bin/login'
+
+send_url = base_url + 'cgi-bin/singlesend'
+
+message_url = base_url + 'cgi-bin/message'
+
+send_page_url = base_url + 'cgi-bin/singlesendpage'
+
+common_headers = tornado.httputil.HTTPHeaders(
+    {
+        "Connection": "keep-alive",
+        "Origin": base_url,
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36",
+        "Accept-Encoding": "gzip,deflate,sdch",
+        "Accept-Language": "zh-CN,zh;q=0.8"
+    })
 
 
 class CookieManager(object):
 
     def __init__(self):
-        self.cookie = {}
+        self.cookie = {'data_bizuin': '2393201154', 'slave_user': 'gh_d188e2888313',
+                       'bizuin': '2391209664'}
 
     def is_empty(self):
-        return not self.cookie
+        return self.cookie.get('slave_sid', '') == '' or self.cookie.get('data_ticket', '') == ''
 
     def clear(self):
-        self.cookie.clear()
+        self.cookie = {'data_bizuin': '2393201154', 'slave_user': 'gh_d188e2888313',
+                       'bizuin': '2391209664'}
 
     def build(self):
         return ';'.join(['%s=%s' % (key, value) for key, value in self.cookie.items()])
@@ -37,10 +56,11 @@ class CookieManager(object):
         for sc in headers.get_list("Set-Cookie"):
             c = Cookie.SimpleCookie(sc)
             for morsel in c.values():
-                if morsel.value and morsel.value != 'EXPIRED':
-                    self.cookie[morsel.key] = morsel.value
-                else:
-                    self.pop(morsel.key, None)
+                if morsel.key not in ['data_bizuin', 'slave_user', 'bizuin']:
+                    if morsel.value and morsel.value != 'EXPIRED':
+                        self.cookie[morsel.key] = morsel.value
+                    else:
+                        self.cookie.pop(morsel.key, None)
         print 'Cookie updated:', self.cookie
 
 
@@ -50,48 +70,47 @@ class WechatConnector(object):
         self.token = ''
         self.last_login = 0
         self.cookie_manager = CookieManager()
-        self.headers = tornado.httputil.HTTPHeaders(
-            {
-                "Connection": "keep-alive",
-                "Accept": "application/json, text/javascript, */*; q=0.01",
-                "Origin": "https://mp.weixin.qq.com",
-                "X-Requested-With": "XMLHttpRequest",
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36",
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "Referer": "https://mp.weixin.qq.com/",
-                "Accept-Encoding": "gzip,deflate,sdch",
-                "Accept-Language": "zh-CN,zh;q=0.8"
-            })
 
     def has_login(self):
         return self.token and not self.cookie_manager.is_empty() and time.time() - self.last_login < 60 * 10
 
     @tornado.gen.coroutine
-    def post_request(self, url, data):
-        print 'Weixin post %s, data:' % url, data, time.ctime()
+    def post_request(self, url, data, **kwargs):
+        headers = common_headers.copy()
+        headers.add('Cookie', self.cookie_manager.build())
+        headers.add('Accept', 'application/json, text/javascript, */*; q=0.01')
+        headers.add('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+        headers.add('Referer', kwargs.get('referer', base_url))
+
         client = tornado.httpclient.AsyncHTTPClient()
-        self.headers.add('cookie', self.cookie_manager.build())
-        body = urllib.urlencode(data)
+        print 'Weixin POST url: %s\nheaders: %s\ndata: %s' % (url, headers, data)
+
         req = tornado.httpclient.HTTPRequest(
-            url=url, method='POST', headers=self.headers, body=body, connect_timeout=60, request_timeout=60)
+            url=url, method='POST', headers=headers, body=urllib.urlencode(data), connect_timeout=60, request_timeout=60)
         res = yield client.fetch(req)
         if res.code == 200:
             self.cookie_manager.set_cookie(res.headers)
             data = json.loads(res.body, encoding='utf-8')
-            print 'Response: ', data, time.ctime()
+            print 'Response: ', data
             sys.stdout.flush()
             raise tornado.gen.Return(data)
         else:
             raise tornado.gen.Return(None)
 
     @tornado.gen.coroutine
-    def get_request(self, url, data):
+    def get_request(self, url, data, **kwargs):
+        headers = common_headers.copy()
+        headers.add('Cookie', self.cookie_manager.build())
+        headers.add(
+            'Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        headers.add('Referer', kwargs.get('referer', base_url))
+
         client = tornado.httpclient.AsyncHTTPClient()
-        self.headers.add('cookie', self.cookie_manager.build())
         url += '?' + urllib.urlencode(data)
-        print 'Wexin get:', url, time.ctime()
+        print 'Wexin GET url: %s\nheaders: %s' % (url, headers)
+
         req = tornado.httpclient.HTTPRequest(
-            url=url, method='GET', headers=self.headers, connect_timeout=60, request_timeout=60)
+            url=url, method='GET', headers=headers, connect_timeout=60, request_timeout=60)
         res = yield client.fetch(req)
         if res.code == 200:
             self.cookie_manager.set_cookie(res.headers)
@@ -102,16 +121,16 @@ class WechatConnector(object):
     @tornado.gen.coroutine
     def login(self, username, pwd):
         # self.cookie_manager.clear()
-        print 'try login...', time.ctime()
+        print 'try login...'
         result = yield self.post_request(login_url, {'username': username, 'pwd': pwd, 'f': 'json'})
         if result and result['base_resp']['ret'] == 0:
             self.last_login = time.time()
             self.token = dict(
                 urlparse.parse_qsl(result['redirect_url']))['token']
-            print 'login success, new token:', self.token, time.ctime()
+            print 'login success, new token:', self.token
             raise tornado.gen.Return(self.token)
         else:
-            print 'login failed:', time.ctime()
+            print 'login failed'
             raise tornado.gen.Return(None)
 
     def _check_same(self, timestamp, content, mtype, user):
@@ -127,8 +146,10 @@ class WechatConnector(object):
 
     @tornado.gen.coroutine
     def find_user(self, timestamp, content, mtype, count, offset):
+        referer = home_url + '?' + \
+            urllib.urlencode({'t': 'home/index', 'token': self.token, 'lang': 'zh_CN'})
         result = yield self.get_request(message_url, {
-            'count': count, 'offset': offset, 'day': 7, 'token': self.token})
+            'count': count, 'offset': offset, 'day': 7, 'token': self.token}, referer=referer)
         raw = BeautifulSoup(result)
         try:
             t = raw.find_all(
@@ -140,25 +161,25 @@ class WechatConnector(object):
 
         if not users:
             if raw.find('div', {'class': 'msg_content'}).text.strip().startswith(u'\u767b'):
-                print 'find_user failed: login expired', time.ctime()
+                print 'find_user failed: login expired'
                 sys.stdout.flush()
                 raise tornado.gen.Return({'err': 6, 'msg': 'login expired'})
             else:
-                print 'find_user failed', time.ctime()
+                print 'find_user failed'
                 sys.stdout.flush()
                 raise tornado.gen.Return({'err': 4, 'msg': 'fail to find user'})
         for i in range(0, len(users) - 1):
             if users[i]['date_time'] < timestamp:
-                print 'find_user failed: no match 1', time.ctime()
+                print 'find_user failed: no match 1'
                 sys.stdout.flush()
                 raise tornado.gen.Return({'err': 4, 'msg': 'fail to find user'})
             elif self._check_same(timestamp, content, mtype, users[i]):
                 if not self._check_same(timestamp, content, mtype, users[i + 1]):
-                    print 'find_user success: ', users[i], time.ctime()
+                    print 'find_user success: ', users[i]
                     sys.stdout.flush()
                     raise tornado.gen.Return({'err': 0, 'msg': users[i]})
                 else:
-                    print 'find_user failed: no match 2', time.ctime()
+                    print 'find_user failed: no match 2'
                     sys.stdout.flush()
                     raise tornado.gen.Return({'err': 4, 'msg': 'fail to find user'})
         res = yield self.find_user(timestamp, content, mtype, count, count + offset - 1)
@@ -169,10 +190,13 @@ class WechatConnector(object):
         url = send_url + '?' + \
             urllib.urlencode(
                 {'t': 'ajax-response', 'f': 'json', 'token': self.token, 'lang': 'zh_CN'})
+        referer = send_page_url + '?' + \
+            urllib.urlencode(
+                {'tofakeid': fakeid, 't': 'message/send', 'action': 'index', 'token': self.token, 'lang': 'zh_CN'})
         result = yield self.post_request(url, {
             'token': self.token, 'lang': 'zh_CN', 'f': 'json', 'ajax': 1, 'type': 1,
-            'content': content.encode('utf-8'), 'tofakeid': fakeid})
-        print 'send_text_message response', result, time.ctime()
+            'content': content.encode('utf-8'), 'tofakeid': fakeid, 'random': random.random()}, referer=referer)
+        print 'send_text_message response', result
         sys.stdout.flush()
         try:
             if result['base_resp']['ret'] == -3:
